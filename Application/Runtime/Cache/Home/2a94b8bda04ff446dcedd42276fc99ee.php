1f9083b5e1b043cd313da982502d3f4b116a0d38
laotan <tylerflyn@outlook.com>
2016-07-22 19:42:48 +0800

    write index.html

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,34 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf8">
+	<title>简易商城</title>
+	<link rel="stylesheet" type="text/css" href="/git/shop/Public/Home/css/main.css">
+	<script type="text/javascript" src="/git/shop/Public/Home/js/jquery-3.0.0.min.js"></script>
+	<script type="text/javascript" src="/git/shop/Public/Home/js/main.js"></script>
+	</head>
+<body>
+<div id="contain">
+	<div id="top">
+		<div id="top_1">
+			<span class="left"><a href="">商品入驻</a></span>
+			<span class="right"><a href="">注册</a>&nbsp;&nbsp;<a href="">登陆</a>&nbsp;&nbsp;&nbsp;&nbsp;<b>|</b>&nbsp;&nbsp;&nbsp;&nbsp;<a href="">我的订单</a></span>
+		</div>
+		<div id="top_2">
+			<img src="/git/shop/Public/Home/img/index/top_ad.jpg">
+		</div>
+		<div id="top_3" >
+			<img id="logo" src="/git/shop/Public/Home/img/index/logo.png">
+			<input id="search_kw"></input>
+			<input id="search_btn" type="submit" value=""></input>
+			<a href=""><div>我的购物车(0)</div></a>
+		</div>
+	</div>
+	<div id="main">
+	</div>
+	<div id="foot">
+	</div>
+</div>
+
+</body>
+</html>
---
+++ Public/Home/js/main.js
@@ -0,0 +1,5 @@
+$(function() {
+ 	$("button").click(function(){
+  		alert('nihao');   
+ 	});
+});
